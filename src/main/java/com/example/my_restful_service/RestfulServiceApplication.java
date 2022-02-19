91c94147d6f4ce7f8d2bab1d826a823a942a4410
leewoonhyoung <wh3619@gmail.com>
2022-02-19 22:46:59 +0900

    start Rest API

---
+++ src/main/java/com/example/my_restful_service/RestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_restful_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulServiceApplication.class, args);
+    }
+
+}
